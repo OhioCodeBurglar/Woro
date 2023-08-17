6efc5de831eae7d706cb70a888021243a42008fb
Jose <jose_antonio_mella@outlook.com>
2023-08-17 18:22:40 -0400

    Added fishing and hunger parameters

--- WORO.py
+++ WORO.py
@@ -6,6 +6,11 @@ import threading
 import time
 import queue
 import curses
+import datetime
+import random
+food = 10
+dizziness = 0
+holes = 0
 
 WIND_KEY = "cb74d8052fc04a7f847bb7cdf2450ef9"
 WAVE_KEY = "AzbGQWqzFYqgIXMssbgaimmCMcvsXyor"
@@ -20,7 +25,7 @@ def logo(stdscr):
     curses.noecho()
     stdscr.keypad(True)
     stdscr.clear()
-
+    curses.curs_set(0)  # Hide the cursor
     screen_height,screen_width = stdscr.getmaxyx()
 
     # ASCII art for the logo
@@ -44,6 +49,22 @@ def logo(stdscr):
     # Refresh the screen
     stdscr.refresh()
     curses.napms(4200)
+
+def die(stdscr,cause:str):
+    curses.cbreak()
+    curses.noecho()
+    stdscr.keypad(True)
+    curses.curs_set(0)  # Hide the cursor
+    while not stop_event.is_set():
+        died_art = """██╗░░░██╗░█████╗░██╗░░░██╗  ██████╗░██╗███████╗██████╗░
+        ╚██╗░██╔╝██╔══██╗██║░░░██║  ██╔══██╗██║██╔════╝██╔══██╗
+        ░╚████╔╝░██║░░██║██║░░░██║  ██║░░██║██║█████╗░░██║░░██║
+        ░░╚██╔╝░░██║░░██║██║░░░██║  ██║░░██║██║██╔══╝░░██║░░██║
+        ░░░██║░░░╚█████╔╝╚██████╔╝  ██████╔╝██║███████╗██████╔╝
+        ░░░╚═╝░░░░╚════╝░░╚═════╝░  ╚═════╝░╚═╝╚══════╝╚═════╝░"""
+        stdscr.clear()
+        stdscr.addstr(0,0,died_art)
+        stdscr.addstr(7,0,cause)
 
 def fetch_wind_data():
     while not stop_event.is_set():
@@ -100,13 +121,13 @@ def estimate_wind(wind_speed, wind_direction):
         28.3: "violent storm",
     }
     direction_labels = {
-        (337.5, 22.5): "north",
+        (337.5, 22.5): "north      ",
         (22.5, 67.5): "northeast",
-        (67.5, 112.5): "east",
+        (67.5, 112.5): "east      ",
         (112.5, 157.5): "southeast",
-        (157.5, 202.5): "south",
+        (157.5, 202.5): "south      ",
         (202.5, 247.5): "southwest",
-        (247.5, 292.5): "west",
+        (247.5, 292.5): "west      ",
         (292.5, 337.5): "northwest"
     }
 
@@ -141,14 +162,46 @@ def get_ocean(latitude, longitude):
             return "South Pacific Ocean"
     else:
         return "Unknown"
+def fish_population(ocean,wind_speed,latitude):
+    #GET MONTH
+    month = datetime.datetime.now().month
+    #TEMPERATURE CALCULATIONS
+    base_temperature = 20  # A rough starting point
+    latitude_adjustment = abs(latitude) / 50  # Normalize latitude to a 0-1 range
 
+    if month >= 4 and month <= 9:  # Warmer months
+        temperature_adjustment = 0.5
+    else:
+        temperature_adjustment = 0
+
+    estimated_temperature = base_temperature + latitude_adjustment + temperature_adjustment
+    fish_population = 0.1*wind_speed+0.01*month+0.001*latitude+(estimated_temperature/10)
+    fish_population = (fish_population - 0) / (20 - 0)
+    # Map normalized value to probability range (e.g., 0% to 100%)
+    mapped_probability = fish_population * 100
+
+    # Simulate the chance of catching a fish
+    random_number = random.uniform(0, 100)
+    if random_number <= mapped_probability:
+        return 1
+    else:
+        return 0
+def hunger(stdscr):
+    global food
+    time.sleep(30)
+    if food <= 1:
+        die(stdscr,"You died because of starvation,next time fish more, or go to more fish friendly areas")
+    else:
+        food-=1
 def main(stdscr):
-    global latitude, longitude
+    global latitude, longitude,food,dizziness,holes
     curses.cbreak()
     curses.noecho()
     stdscr.keypad(True)
-    stdscr.clear() 
+    curses.curs_set(0)  # Hide the cursor
 
+    stdscr.clear() 
+    is_fishing = False
     wind_speed = 0.0
     # Define the precise_round function
     def precise_round(value):
@@ -162,7 +215,10 @@ def main(stdscr):
     # Create a thread for fetching wind data
     wind_thread = threading.Thread(target=fetch_wind_data)
     wind_thread.start()
-
+    
+    #Create a food depletion thread
+    hunger_thread = threading.Thread(target=hunger,args=(stdscr,))
+    hunger_thread.start()
     # Define a dictionary to map key presses to velocity changes
     key_velocity_mapping = {
         ord('h'): (0.0,0.0), #anchored
@@ -185,8 +241,8 @@ def main(stdscr):
             try:
                 wind_speed, wind_direction = print_queue.get_nowait()
                 wind_label,direction_label = estimate_wind(wind_speed,wind_direction)
-                stdscr.addstr(0, 0, f"{wind_label} coming from the {direction_label}")
-                stdscr.addstr(1, 0, '----------------------------------')
+                stdscr.addstr(1, 0, f"{wind_label} coming from the {direction_label}")
+                stdscr.addstr(2, 0, '----------------------------------')
             except queue.Empty:
                 pass
 
@@ -198,25 +254,30 @@ def main(stdscr):
             
             # Define the direction text
             direction_text = {
-                (0, 0): 'anchored',
-                (1, 0): 'going east',
-                (-1, 0): 'going west',
-                (0, 1): 'going north',
-                (0, -1): 'going south',
-                (1, 1): 'going northeast',
-                (-1, 1): 'going northwest',
-                (1, -1): 'going southeast',
-                (-1, -1): 'going southwest'
+                (0, 0): 'anchored            ',
+                (1, 0): 'going east         ',
+                (-1, 0): 'going west        ',
+                (0, 1): 'going north    ',
+                (0, -1): 'going south    ',
+                (1, 1): 'going northeast    ',
+                (-1, 1): 'going northwest    ',
+                (1, -1): 'going southeast    ',
+                (-1, -1): 'going southwest    '
             }
             wave_height = waveHeight(wind_speed)
             ocean = get_ocean(longitude,latitude)
             visual_velocity = (round(velocity[0],2),round(velocity[1],2))
-            stdscr.addstr(2, 0, direction_text[rounded_velocity])
-            stdscr.addstr(3, 0, f"x velocity: {visual_velocity[0]}")
-            stdscr.addstr(4, 0, f"y velocity: {visual_velocity[1]}")
-            stdscr.addstr(5,0, '----------------------------------')
-            stdscr.addstr(6,0, f"average wave height:{wave_height}m")
-            stdscr.addstr(7,0, f"Current Ocean: {ocean}")
+            stdscr.addstr(0,0, "Boat Info:")
+            stdscr.addstr(3, 0, direction_text[rounded_velocity])
+            stdscr.addstr(4, 0, f"x velocity: {visual_velocity[0]}")
+            stdscr.addstr(5, 0, f"y velocity: {visual_velocity[1]}")
+            stdscr.addstr(6,0, '----------------------------------')
+            stdscr.addstr(7,0, f"average wave height:{wave_height}m")
+            stdscr.addstr(8,0, f"Current Ocean: {ocean}")
+            stdscr.addstr(9,0, '----------------------------------') 
+            stdscr.addstr(10,0, "Personal Info")
+            stdscr.addstr(11,0, f"Food: {food}")
+            stdscr.addstr(12,0, f"Dizziness: {dizziness}") 
             stdscr.refresh()
 
 
@@ -224,16 +285,38 @@ def main(stdscr):
             key = stdscr.getch()
             if key == ord('q'):
                 stop_event.set()  # Signal the other thread to terminate
-                wind_thread.join()  # Wait for the wind thread to finish before exiting
+                wind_thread.join()# Wait for the wind thread to finish before exiting
+                hunger_thread.join()#same for the hunger thread
+                break
+            elif key == ord('+'):
+                stop_event.set()
                 break
             elif key in key_velocity_mapping:
                 velocity = key_velocity_mapping[key]
-            
+            elif key == ord('*'):
+                if is_fishing:
+                    stdscr.addstr(13, 0, "")
+                    is_fishing = False
+                else:
+                    velocity = (0.0, 0.0)
+                    stdscr.addstr(13, 0, "Fishing...       ")
+                    stdscr.refresh()
+                    stdscr.nodelay(False)  # Disable non-blocking getch
+                    if fish_population(ocean, wind_speed, latitude) == 1:
+                        fish_catch_message = "You caught a fish!"
+                        food += 1
+                    else:
+                        fish_catch_message = ""
+                    stdscr.nodelay(True)  # Re-enable non-blocking getch
+                    is_fishing = True
+                    time.sleep(3)
+                    stdscr.addstr(13, 0, fish_catch_message)
+                    stdscr.refresh()
 
             time.sleep(1)  # Allow some time for the other thread to print data
 
     except KeyboardInterrupt:
-        print("Exiting...")
+        print("Exiting...       ")
 
     finally:
         curses.nocbreak()
